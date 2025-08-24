6f6c5ac5a96f1b75628d88e652b0b010c98b0ddb
ZHANG Yuntian <yt@radxa.com>
2025-08-24 11:13:55 +0800

    fix: memory is actually 8G
    Change-Id: I73fa8094db22912f648ae46c79640fdd044d9ebf

--- Platform/Radxa/Orion/O6T/mem_config/default/GlobalConfig.c
+++ Platform/Radxa/Orion/O6T/mem_config/default/GlobalConfig.c
@@ -11,21 +11,21 @@ MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock = {
   .MaxFreq          = DDR5500_FREQUENCY,
   .ChMask           = 0xF,
   .DdrType          = DDR_TYPE_LPDDR5,
-  .DeviceDensity    = 12,
+  .DeviceDensity    = 6,
   .DeviceWidth      = 16,
-  .RankNum          = 2,
+  .RankNum          = 1,
 };
 
-MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_24G = {
+MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_16G = {
   {
     .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
     .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = ORION_O6T_24G_MASK
+    .BoardMask      = ORION_O6T_16G_MASK
   },
   .MaxFreq          = DDR3200_FREQUENCY,
   .ChMask           = 0xF,
   .DdrType          = DDR_TYPE_LPDDR5,
-  .DeviceDensity    = 12,
+  .DeviceDensity    = 8,
   .DeviceWidth      = 16,
   .RankNum          = 2,
 };
