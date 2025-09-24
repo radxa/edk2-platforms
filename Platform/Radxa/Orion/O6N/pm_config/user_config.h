0b68b3492b7a625b71e8d9081fcb887ed210bca5
ZHANG Yuntian <yt@radxa.com>
2025-09-24 07:47:49 +0000

    fix: update fan PWM frequency
    Change-Id: If0e2b3af527565ec32752cff2b328a8886187bc1

--- Platform/Radxa/Orion/O6N/pm_config/user_config.h
+++ Platform/Radxa/Orion/O6N/pm_config/user_config.h
@@ -165,7 +165,7 @@ static pm_config_fan_t fan_config[MAX_FAN_NUM] = {
         .pwm_freq = {
             .fields = {
                 .valid = PM_CONFIG_VALID,
-                .raw_data = 1000, // the pwm frequence. depending on the fan's characteristic, 50~1000 is recommended.
+                .raw_data = 25000, // the pwm frequence. depending on the fan's characteristic, 50~1000 is recommended.
             }
         }
     },
