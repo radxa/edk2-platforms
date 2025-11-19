09e6b3de86918770d177da19348dd86fd0345c8e
ZHANG Yuntian <yt@radxa.com>
2025-11-19 11:06:40 +0000

    Platform/O6N: update memcfg
    Based on mem_config_O6N 20251124.zip
    
    Signed-off-by: ZHANG Yuntian <yt@radxa.com>
    Change-Id: I7a1bde5e74903973d07fc80b2201c48d3d5961d7

--- Platform/Radxa/Orion/O6N/mem_config/RS600/GlobalConfig.c
+++ Platform/Radxa/Orion/O6N/mem_config/RS600/GlobalConfig.c
@@ -2,86 +2,31 @@
 #include "MemConfigBinTool.h"
 #include "MemConfigurationTable.h"
 
-MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_8G = {
+
+MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_16G_HS = {
   {
     .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
     .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_8G_MASK
+    .BoardMask      = RS602_16G_HS_MASK
   },
-  .MaxFreq          = MEM_CFG_MEMFREQ,
+  .MaxFreq          = DDR5000_FREQUENCY,
   .ChMask           = 0xF,
   .DdrType          = DDR_TYPE_LPDDR5,
   .DeviceDensity    = 8,
   .DeviceWidth      = 16,
-  .RankNum          = 1,
-};
-
-MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_12G = {
-  {
-    .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
-    .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_12G_MASK
-  },
-  .MaxFreq          = MEM_CFG_MEMFREQ,
-  .ChMask           = 0xF,
-  .DdrType          = DDR_TYPE_LPDDR5,
-  .DeviceDensity    = 12,
-  .DeviceWidth      = 16,
-  .RankNum          = 1,
-};
-
-MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_16G = {
-  {
-    .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
-    .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_16G_MASK
-  },
-  .MaxFreq          = MEM_CFG_MEMFREQ,
-  .ChMask           = 0xF,
-  .DdrType          = DDR_TYPE_LPDDR5,
-  .DeviceDensity    = 16,
-  .DeviceWidth      = 16,
-  .RankNum          = 1,
+  .RankNum          = 2,
 };
 
 MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_24G_HS = {
   {
     .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
     .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_24G_HS_MASK
+    .BoardMask      = RS602_24G_HS_MASK
   },
-  .MaxFreq          = DDR5500_FREQUENCY,
+  .MaxFreq          = DDR5000_FREQUENCY,
   .ChMask           = 0xF,
   .DdrType          = DDR_TYPE_LPDDR5,
   .DeviceDensity    = 12,
-  .DeviceWidth      = 16,
-  .RankNum          = 2,
-};
-
-MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_32G = {
-  {
-    .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
-    .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_32G_MASK
-  },
-  .MaxFreq          = MEM_CFG_MEMFREQ,
-  .ChMask           = 0xF,
-  .DdrType          = DDR_TYPE_LPDDR5,
-  .DeviceDensity    = 16,
-  .DeviceWidth      = 16,
-  .RankNum          = 2,
-};
-
-MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_32G_Lo = {
-  {
-    .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
-    .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_32G_LO_MASK
-  },
-  .MaxFreq          = DDR4800_FREQUENCY,
-  .ChMask           = 0xF,
-  .DdrType          = DDR_TYPE_LPDDR5,
-  .DeviceDensity    = 16,
   .DeviceWidth      = 16,
   .RankNum          = 2,
 };
@@ -90,9 +35,9 @@ MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_32G_x8_HS = {
   {
     .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
     .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_32G_x8_HS_MASK
+    .BoardMask      = RS602_32G_x8_HS_MASK
   },
-  .MaxFreq          = DDR5500_FREQUENCY,
+  .MaxFreq          = DDR5000_FREQUENCY,
   .ChMask           = 0xF,
   .DdrType          = DDR_TYPE_LPDDR5,
   .DeviceDensity    = 8,
@@ -104,7 +49,7 @@ MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_32G_Hynix = {
   {
     .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
     .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_32G_HYNIX_MASK
+    .BoardMask      = RS602_32G_HYNIX_MASK
   },
   .MaxFreq          = DDR6000_FREQUENCY,
   .ChMask           = 0xF,
@@ -118,26 +63,12 @@ MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_48G_HS = {
   {
     .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
     .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_48G_HS_MASK
+    .BoardMask      = RS602_48G_HS_MASK
   },
-  .MaxFreq          = DDR5500_FREQUENCY,
+  .MaxFreq          = DDR5000_FREQUENCY,
   .ChMask           = 0xF,
   .DdrType          = DDR_TYPE_LPDDR5,
   .DeviceDensity    = 12,
-  .DeviceWidth      = 8,
-  .RankNum          = 2,
-};
-
-MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_64G = {
-  {
-    .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
-    .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_64G_x8_MASK
-  },
-  .MaxFreq          = DDR5500_FREQUENCY,
-  .ChMask           = 0xF,
-  .DdrType          = DDR_TYPE_LPDDR5,
-  .DeviceDensity    = 16,
   .DeviceWidth      = 8,
   .RankNum          = 2,
 };
@@ -146,26 +77,12 @@ MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_64G_Rayson = {
   {
     .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
     .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_64G_RAYSON_MASK
-  },
-  .MaxFreq          = DDR5500_FREQUENCY,
-  .ChMask           = 0xF,
-  .DdrType          = DDR_TYPE_LPDDR5,
-  .DeviceDensity    = 16,
-  .DeviceWidth      = 8,
-  .RankNum          = 2,
-};
-
-MEM_CONFIG_BLOCK_CONFIG  GlobalConfigBlock_16G_HS = {
-  {
-    .Signature      = MEM_CONFIG_BLOCK_CONFIG_SIGNAUTE,
-    .BlockSize      = sizeof(MEM_CONFIG_BLOCK_CONFIG),
-    .BoardMask      = RS600_16G_HS_MASK
+    .BoardMask      = RS602_64G_RAYSON_MASK
   },
   .MaxFreq          = DDR4800_FREQUENCY,
   .ChMask           = 0xF,
   .DdrType          = DDR_TYPE_LPDDR5,
-  .DeviceDensity    = 8,
-  .DeviceWidth      = 16,
+  .DeviceDensity    = 16,
+  .DeviceWidth      = 8,
   .RankNum          = 2,
 };
