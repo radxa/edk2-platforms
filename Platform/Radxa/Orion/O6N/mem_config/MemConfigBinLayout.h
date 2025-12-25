6b3fc1ab655753e0762cd9fc57d9cf2c3940ef30
Zhaoming Luo <luozhaoming@radxa.com>
2025-12-25 10:09:08 +0800

    Update Hive Semi 48G mem config
    Configure as DDR5000, disable FFE, update trace length.

--- Platform/Radxa/Orion/O6N/mem_config/MemConfigBinLayout.h
+++ Platform/Radxa/Orion/O6N/mem_config/MemConfigBinLayout.h
@@ -9,10 +9,12 @@ extern MEM_CONFIG_BUSCFG_LP5        MemLpddr5BusCfg;
 extern MEM_CONFIG_BUSCFG_LP5        MemLpddr5BusCfg_Hive_Semi_48G;
 extern MEM_CONFIG_PHYPADCFG         MemPhyPadCfg;
 extern MEM_CONFIG_PHYPADCFG         MemPhyPadCfg_Hive_Semi;
+extern MEM_CONFIG_PHYPADCFG         MemPhyPadCfg_Hive_Semi_48G;
 extern MEM_CONFIG_BIOS_SETUP        MemBiosSetup;
 extern MEM_CONFIG_DQ_SWAP           DqDmMapBlock;
 extern MEM_CONFIG_TRAIN_OPTIMIZE    MemTrainOptimize;
 extern MEM_CONFIG_TRACE_LEN_DIFF    MemTraceLenDiffCfg;
+extern MEM_CONFIG_TRACE_LEN_DIFF    MemTraceLenDiffCfg_Hive_Semi_48G;
 extern MEM_CONFIG_BDID_GET_METHOD   BoardIdGetMethod;
 
 extern MEM_CONFIG_BLOCK_CONFIG      GlobalConfigBlock_64G_Rayson;
@@ -34,9 +36,11 @@ extern MEM_CONFIG_BLOCK_CONFIG      GlobalConfigBlock_16G_HS;
   CDCB_BLOCK(MemLpddr5BusCfg_Hive_Semi_48G), \
   CDCB_BLOCK(MemPhyPadCfg), \
   CDCB_BLOCK(MemPhyPadCfg_Hive_Semi), \
+  CDCB_BLOCK(MemPhyPadCfg_Hive_Semi_48G), \
   CDCB_BLOCK(DqDmMapBlock), \
   CDCB_BLOCK(MemTrainOptimize), \
   CDCB_BLOCK(MemTraceLenDiffCfg), \
+  CDCB_BLOCK(MemTraceLenDiffCfg_Hive_Semi_48G), \
   CDCB_BLOCK(GlobalConfigBlock_64G_Rayson), \
   CDCB_BLOCK(GlobalConfigBlock_24G_HS), \
   CDCB_BLOCK(GlobalConfigBlock_32G_x8_HS), \
